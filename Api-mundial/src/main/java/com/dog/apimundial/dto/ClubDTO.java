e78b1d44a5482264e501d5562fd9b02fb7472d6c
ArielaClaraG <00097725@uca.edu.sv>
2025-06-14 09:36:26 -0600

    add: ClubEntity

--- Api-mundial/src/main/java/com/dog/apimundial/dto/ClubDTO.java
+++ Api-mundial/src/main/java/com/dog/apimundial/dto/ClubDTO.java
@@ -1,4 +1,13 @@
 package com.dog.apimundial.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+@Entity
 public class ClubDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String country;
+    private int titles;
 }
