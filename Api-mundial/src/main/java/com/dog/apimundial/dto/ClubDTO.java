8686f05b697b5b31727c1738b5573888d4617505
ArielaClaraG <00097725@uca.edu.sv>
2025-06-14 09:39:14 -0600

    add: ClubEntity-fix

--- Api-mundial/src/main/java/com/dog/apimundial/dto/ClubDTO.java
+++ Api-mundial/src/main/java/com/dog/apimundial/dto/ClubDTO.java
@@ -1,13 +1,7 @@
 package com.dog.apimundial.dto;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+
 @Entity
 public class ClubDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String country;
-    private int titles;
+
 }
--- Api-mundial/src/main/java/com/dog/apimundial/entities/Club.java
+++ Api-mundial/src/main/java/com/dog/apimundial/entities/Club.java
@@ -1,4 +1,15 @@
 package com.dog.apimundial.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+
 public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String country;
+    private int titles;
 }
