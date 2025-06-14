eef37635643bdd7eb3e644ea1ddc5510f789e84d
V1ctorHUB <00046921@uca.edu.sv>
2025-06-14 09:56:21 -0600

    add: ClubRepository

--- Api-mundial/src/main/java/com/dog/apimundial/repository/ClubRepository.java
+++ Api-mundial/src/main/java/com/dog/apimundial/repository/ClubRepository.java
@@ -1,4 +1,15 @@
 package com.dog.apimundial.repository;
 
-public class ClubRepository {
-}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.dog.apimundial.entities.Club;
+import com.dog.apimundial.dto.ClubDTO;
+
+@Repository
+public interface ClubRepository extends JpaRepository<Club,Long> {
+
+    List<ClubDTO> findByTitles(int titles);
+    
+} 
